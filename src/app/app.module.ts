8a2e6d0bdcba30c23ba19d1b342c302c627d16be
Dennis <dennisndegwa001@gmail.com>
2023-08-22 15:28:05 +0300

    Change Id a bit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -59,7 +59,7 @@ const googleLoginOptions: GoogleInitOptions = {
         providers: [
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('868521'),
+            provider: new FacebookLoginProvider('869422684538522'),
           },
         ],
       } as SocialAuthServiceConfig,
