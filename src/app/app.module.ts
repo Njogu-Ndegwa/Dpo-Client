61494fbdba138588376cf98ea01ca653f30e3798
Dennis <dennisndegwa001@gmail.com>
2023-08-22 15:15:54 +0300

    Latest Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -59,7 +59,7 @@ const googleLoginOptions: GoogleInitOptions = {
         providers: [
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('869422684538521'),
+            provider: new FacebookLoginProvider('868521'),
           },
         ],
       } as SocialAuthServiceConfig,
