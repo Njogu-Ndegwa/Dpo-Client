f3f6a3539caa914e6844f74e59b7bc2e4f24d0a7
Dennis <dennisndegwa001@gmail.com>
2023-08-23 09:23:53 +0300

    Add

--- src/app/pages/payment/verify-payment/verify-payment.component.ts
+++ src/app/pages/payment/verify-payment/verify-payment.component.ts
@@ -49,13 +49,29 @@ export class VerifyPaymentComponent implements OnInit {
 
   testFunction() {
   // @ts-ignore
-window.publishOverlayAPI.connectDomain()
+// window.publishOverlayAPI.connectDomain()
 
 // // @ts-ignore
 // console.log(window.publishOverlayAPI)
 
 // // @ts-ignore
 // console.log(window.publishOverlayAPI.connectDomain())
+
+function getParameterByName(name:any, url:any) {
+  if (!url) {
+      url = window.location.href;
+  }
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+// @ts-ignore
+window.parent.postMessage({ key: 'publish-overlay-connect-domain' }, getParameterByName('editorOrigin'));
+
   }
 
 
