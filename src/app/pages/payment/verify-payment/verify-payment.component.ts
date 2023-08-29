03370b76c57c042fc26e6c499ec529f0b956aa31
Dennis <dennisndegwa001@gmail.com>
2023-08-29 14:29:55 +0300

    Add Loader to Verification Page

--- src/app/pages/payment/verify-payment/verify-payment.component.ts
+++ src/app/pages/payment/verify-payment/verify-payment.component.ts
@@ -14,6 +14,7 @@ import { SharedService } from 'src/app/services/iframe/iframe-control.service';
 export class VerifyPaymentComponent implements OnInit {
   @Output() callFunctionInA = new EventEmitter<void>();
   transactionToken: any
+  isLoading: boolean = false
   constructor(
     private activatedRoute: ActivatedRoute,
     private paymentService: PaymentsService,
@@ -27,19 +28,23 @@ export class VerifyPaymentComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    this.verifyPayment()
   }
 
 
   verifyPayment() {
     console.log(this.transactionToken, 'The Transaction Token')
+    this.isLoading = true
     this.paymentService.verifyPaymentService(this.transactionToken).subscribe((res: any) => {
       console.log(res, 'The Result')
       if (res['status'] === 'paid') {
-        Notify.success('Payment Succesfult')
+        Notify.success('Payment Successful')
         if (res['amount'] === '5500.00') {
 
         } else if (res['amount'] === '7750.00') {
           setTimeout(() => {
+            this.isLoading = false
             this.initiateConnectDomain()
           }, 1000)
         }
